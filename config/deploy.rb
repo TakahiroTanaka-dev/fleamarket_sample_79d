8ffb916360abd43aa51c4d2da975efae66be4481
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-07-22 15:49:32 +0900

    Deploy setting fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.14.1'
 
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'fleamarket_sample_79d'
-set :repo_url,  'git@github.com:githubのユーザー名/fleamarket_sample_79d.git'
+set :repo_url,  'git@github.com:TakahiroTanaka-dev/fleamarket_sample_79d.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
