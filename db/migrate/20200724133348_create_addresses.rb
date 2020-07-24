e150760e49fa651927b428e3c35fd263f5e840a1
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-24 23:20:39 +0900

    addressesテーブル・モデルの作成

---
+++ db/migrate/20200724133348_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :block, null: false
+      t.string :building
+      t.string :tel
+      t.timestamps
+    end
+  end
+end
