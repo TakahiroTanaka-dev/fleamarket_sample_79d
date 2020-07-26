540bdced1885a5a9edc5a157ea86175d4ae81a01
kingranking <switchmario@i.softbank.jp>
2020-07-26 11:15:33 +0900

    [マークアップ]商品購入確認ページ

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ get 'items/index'
 root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :transacts
+resources :addresses
+resources :cards
 end
