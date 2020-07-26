3863ddf6f2584e2b67f29a7fd1486d09c4adf76d
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-07-26 11:03:08 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ get 'items/index'
 # after
 root 'items#index'
 
-resources :items
+resources :items, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
