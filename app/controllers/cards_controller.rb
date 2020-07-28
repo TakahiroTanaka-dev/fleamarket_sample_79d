19857cdd17545047344c08a3131dc141398d1021
kingranking <switchmario@i.softbank.jp>
2020-07-28 16:15:57 +0900

    cards_controller.rb newアクション

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,21 @@
 class CardsController < ApplicationController
 
+  require "payjp"
+
   def new
-    
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
+  end
+
+  def pay
+  
+  end
+
+  def delete
 
   end
 
-  def create
+  def show
 
   end
   
