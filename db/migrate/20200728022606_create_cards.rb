aceaa9d264d407e6a90aa8113a8a6d7f7cdfe37f
kingranking <switchmario@i.softbank.jp>
2020-07-28 12:07:14 +0900

    cradsテーブル作成

--- db/migrate/20200728022606_create_cards.rb
+++ db/migrate/20200728022606_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.string :customer_id
-      t.string :card_id
+      t.references :user, null: false, foreign_key: true
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
 
       t.timestamps
     end
