36f07209cf6770d7586851182210df63d70c5234
kingranking <switchmario@i.softbank.jp>
2020-07-29 11:12:47 +0900

    cards_controllerの作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,15 +8,40 @@ class CardsController < ApplicationController
   end
 
   def pay
-  
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(card: params['payjp-token'])
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
   end
 
   def delete
-
+    card = Card.find_by(user_id: current_user.id)
+    unless card.blank?
+      Payjp.api_key = ENV["PAYJP"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+    redirect_to action: "new"
   end
 
   def show
-
+    card = Card.find_by(user_id: current_user.id)
+    if carc.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrive(card.card_id)
+    end
   end
   
 end
