8689ed0e5ca3e3fcf876d17f1d6c83bb3b5265e5
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-29 12:32:40 +0900

    バリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, :block, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
-  has_many :items, dependent: :destroy
-  has_one :address, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_one :card, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items, dependent: :destroy
+  has_one :address, dependent: :destroy
+  # has_many :comments, dependent: :destroy
+  # has_many :favorites, dependent: :destroy
+  has_one :card, dependent: :destroy
+  
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, confirmation: true, length: { minimum: 7 }
+  validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX , message: "全角のみ使えます" }
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: "全角カタカナのみ使えます"}
+  validates :birth_year, :birth_month, :birth_date, presence: true
 end
