4b5a7ed6316647bde5e7b7e58f9552568811bb35
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-30 11:15:15 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,9 @@ class User < ApplicationRecord
   has_one :card, dependent: :destroy
   
   
-  validates :nickname, presence: true
+  validates :nickname, :birth_day, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, confirmation: true, length: { minimum: 7 }
   validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX , message: "全角のみ使えます" }
   validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: "全角カタカナのみ使えます"}
-  validates :birth_day, presence: true
 end
