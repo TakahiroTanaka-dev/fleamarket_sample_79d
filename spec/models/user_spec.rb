aa1ee2a86387c552c707d2d9db1e12727616bebe
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-31 10:56:11 +0900

    factory_bot導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
