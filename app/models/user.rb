bfaea7eab8b6e5212a83cb9d636f4d4c399754fe
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-31 10:15:20 +0900

    修正
    hamlに変換
    userモデルのコメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
 
   has_many :items, dependent: :destroy
   has_one :address, dependent: :destroy
-  # has_many :comments, dependent: :destroy
-  # has_many :favorites, dependent: :destroy
   has_one :card, dependent: :destroy
   
   
