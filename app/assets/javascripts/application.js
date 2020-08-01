14e56834ed89cf8944f16fbe32962a68fb924ca1
kingranking <switchmario@i.softbank.jp>
2020-08-01 15:42:32 +0900

    カード登録機能の実装
    環境変数をセット

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require rails-ujs
+//= require_tree .
+//= require jquery 
+//= require jquery_ujs 
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ resources :items, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :transacts
 resources :addresses
-resources :cards
+resources :cards, only: [:new, :show, :destroy] do
+  collection do
+    post 'pay', to: 'cards#pay'
+  end
+end
+
 end
