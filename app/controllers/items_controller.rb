803ef5b2470ea6480ccdc2407cd0c87724525c69
MaruSyun <magirucchi.dente117@gmail.com>
2020-08-01 14:38:16 +0900

    商品詳細ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+
   def index
     @categoryitems = Item.all.order("RAND()")
     @branditems = Item.all.order("id DESC")
@@ -14,6 +16,17 @@ class ItemsController < ApplicationController
 
 
   def show
+  end
+
+  private
+  def item_params
+    params.require(:item)
+      .permit(:name, :description, :condition, :category_id, :shipping_cost, :condition, :price, :shipping_id, :prefecture_id, :shipping_day, images_attributes:[:image])
+      .merge(seller_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
