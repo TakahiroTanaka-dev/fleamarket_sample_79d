1f1ef029c1c1809818cb8602baa1446352f27d45
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-08-01 12:45:53 +0900

    画像の保存先を分岐

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
     storage :fog
   # storage :fog
 
