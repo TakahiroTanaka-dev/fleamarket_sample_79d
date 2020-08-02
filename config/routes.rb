1fabc514fe37155a3dae84a4fa3cd7adf918492c
kingranking <switchmario@i.softbank.jp>
2020-08-02 11:59:06 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,24 +8,23 @@ Rails.application.routes.draw do
   end
   # before
 
-get 'items/index'
-# after
-root 'items#index'
+  get 'items/index'
+  # after
+  root 'items#index'
 
-resources :items, only: [:index, :show, :new, :create]
-resources :users, only: :show do
-  member do
-    get 'log_out'
+  resources :items, only: [:index, :show, :new, :create]
+  resources :users, only: :show do
+    member do
+      get 'log_out'
+    end
   end
-end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :transacts
-resources :addresses
-resources :cards, only: [:new, :show, :destroy] do
-  collection do
-    post 'pay', to: 'cards#pay'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :transacts
+  resources :cards, only: [:new, :show, :destroy] do
+    collection do
+      post 'pay', to: 'cards#pay'
+    end
   end
-end
 
 end
