ee5aa6ab9b2dda599a7e8456488807682152edb1
junya1985 <road.mj1998@gmail.com>
2020-08-03 11:47:17 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,13 @@ class ItemsController < ApplicationController
     @categoryitems = Item.all.order("RAND()")
     @branditems = Item.all.order("id DESC")
   end
-
+  
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+  
   def new
     
   end
