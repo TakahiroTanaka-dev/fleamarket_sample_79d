3293c2055c8b8512eac4a849489bec8d644484ad
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-08-05 00:02:39 +0900

    itemのルーティングにajaxで動くルートをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,13 @@ get 'items/index'
 # after
 root 'items#index'
 
-resources :items, only: [:index, :show, :new, :create]
+resources :items, only: [:index, :show, :new, :create] do
+  collection do
+    get 'get_category_children', defaults: { format: 'json' }
+    get 'get_category_grandchildren', defaults: { format: 'json' }
+  end
+end
+
 resources :users, only: :show do
   member do
     get 'log_out'
