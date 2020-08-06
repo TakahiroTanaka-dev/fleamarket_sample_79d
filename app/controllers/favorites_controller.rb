a938359f5c5883ddf1795503a5cc88746678e6cf
junya1985 <road.mj1998@gmail.com>
2020-08-06 12:46:35 +0900

    非同期でのお気に入り機能

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,29 @@
+class FavoritesController < ApplicationController
+  before_action :set_item, only: [:create, :destroy]
+
+  def index
+    @favorites = Favorite.where(user_id: current_user.id)
+  end
+
+  def create
+    @favorite = Favorite.new(
+      user_id: current_user.id, 
+      item_id: @item.id
+    )
+    @favorite.save
+  end
+
+  def destroy
+    @favorite = Favorite.find_by(
+      user_id: current_user.id, 
+      item_id: @item.id
+    )
+    @favorite.destroy
+  end
+
+  private
+  def set_item
+    @product = Item.find(params[:item_id])
+  end
+end
+
