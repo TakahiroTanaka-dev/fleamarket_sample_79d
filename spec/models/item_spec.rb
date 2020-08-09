f2cbbfc1aea0d9fc7a619222af1c611270dc4539
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-08-09 13:53:52 +0900

    nameカラムのテストコードの追加とuserのテストコードの修正

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+describe Item do
+  describe '#create' do
+    it "is invalid without a name" do
+      item = Item.new(name: "", description: "a", condition: 2, price: 123, category_id: 1, shipping_cost: 1, prefecture_id: 3, shipping_day: 1, seller_id: 1 )
+      item.valid?
+      expect(item.errors[:name]).to include("を入力してください")
+    end
+  end
+end
